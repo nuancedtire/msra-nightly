b77288b5404841fa141b375556b4abac82a46952
nuancedtire <149303031+nuancedtire@users.noreply.github.com>
2024-11-11 07:01:28 +0000

    cardio and derma update

--- next.config.mjs
+++ next.config.mjs
@@ -2,16 +2,8 @@ import nextra from 'nextra'
 
 const withNextra = nextra({
   theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.jsx',
-  defaultShowCopyCode: true,
-  flexsearch: true,
-  staticImage: true,
+  themeConfig: './theme.config.jsx'
 })
 
-export default withNextra({
-  reactStrictMode: true,
-  runtime: 'nodejs20',
-})
-
-// If you have other Next.js configurations, you can pass them as the parameter:
-// export default withNextra({ /* other next.js config */ })
+// Export the default config
+export default withNextra()
